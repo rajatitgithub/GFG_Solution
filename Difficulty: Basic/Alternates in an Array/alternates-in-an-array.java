55dc4938bd54d5067097f9ab7caca1eacef9d1da
Rajat <141062518+rajatitgithub@users.noreply.github.com>
2025-08-16 15:02:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Alternates in an Array/alternates-in-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    // function to print alternate elements of an array
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < arr.length; i += 2){
+            list.add(arr[i]);
+        }
+        return list;
+    }
+}
